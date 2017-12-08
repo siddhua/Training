67bc7daf95b0f4b96d4c0c3e77a159440b958d61
Siddhartha Avula <savula@valuelabs.com>
2017-12-08 18:12:25 +0530

    Adding firstprgrm

---
+++ src/com/java/training/FirstPrgm.java
@@ -0,0 +1,14 @@
+package com.java.training;
+
+public class FirstPrgm {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=30;
+		int c;
+		c=a+b;
+		System.out.println("sum is "+c);
+
+	}
+
+}
